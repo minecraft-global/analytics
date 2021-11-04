885ea32e599e913d144bcc97d47b203825dacca6
Milo Weinberg <iapetus011@gmail.com>
2021-11-04 16:38:14 -0400

    finish stats posting

--- MinecraftGlobalAnalytics/src/main/java/global/minecraft/MinecraftGlobalAnalytics/StatsPoster.java
+++ MinecraftGlobalAnalytics/src/main/java/global/minecraft/MinecraftGlobalAnalytics/StatsPoster.java
@@ -2,12 +2,15 @@ package global.minecraft.MinecraftGlobalAnalytics;
 
 import com.sun.management.OperatingSystemMXBean;
 import org.apache.commons.io.FileUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.bukkit.Server;
 import org.bukkit.World;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.management.ManagementFactory;
 import java.time.Instant;
 import java.time.temporal.ChronoField;
@@ -68,12 +71,22 @@ public class StatsPoster implements Runnable {
 
     private void postStats() {
         String data = fetchStats().toJsonString();
+        HttpPost request = new HttpPost("https://v2api.minecraft.global/serverstats");
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+        request.addHeader("Authorization", authorization);
+        request.addHeader("Content-Type", "application/json");
 
         try {
-            HttpPost request = new HttpPost("https://v2api.minecraft.global/server/{server_id}/stats");
-            request.addHeader("Authorization", authorization);
+            request.setEntity(new StringEntity(data));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault(); CloseableHttpResponse response = httpClient.execute(request)) {
+            assert response.getStatusLine().getStatusCode() == 200;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }

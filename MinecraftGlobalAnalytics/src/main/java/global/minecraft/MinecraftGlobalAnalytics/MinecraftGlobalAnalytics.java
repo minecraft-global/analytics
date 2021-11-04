71cd2f937130fc518fd0f90ccfc30b2a19b11a4e
Milo Weinberg <iapetus011@gmail.com>
2021-11-04 13:08:49 -0400

    add base files

---
+++ MinecraftGlobalAnalytics/src/main/java/global/minecraft/MinecraftGlobalAnalytics/MinecraftGlobalAnalytics.java
@@ -0,0 +1,15 @@
+package global.minecraft.MinecraftGlobalAnalytics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinecraftGlobalAnalytics extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}

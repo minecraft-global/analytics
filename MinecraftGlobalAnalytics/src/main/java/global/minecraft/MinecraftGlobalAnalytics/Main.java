5ba0da8d5b39879ad60b5df58c28928b1b3d0a9d
Milo Weinberg <iapetus011@gmail.com>
2021-11-08 14:37:53 -0500

    detect if token hasn't been set, and stop server if so

--- MinecraftGlobalAnalytics/src/main/java/global/minecraft/MinecraftGlobalAnalytics/Main.java
+++ MinecraftGlobalAnalytics/src/main/java/global/minecraft/MinecraftGlobalAnalytics/Main.java
@@ -1,6 +1,8 @@
 package global.minecraft.MinecraftGlobalAnalytics;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Server;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
@@ -10,10 +12,21 @@ public final class Main extends JavaPlugin {
         saveDefaultConfig();
 
         Server server = getServer();
+
+        FileConfiguration config = getConfig();
+        String authorization = config.getString("serverToken");
+
+        if (authorization.equals(config.getDefaults().getString("serverToken"))) {
+            server.getConsoleSender().sendMessage(ChatColor.RED + "Please set the server token in plugins/MinecraftGlobalAnalytics/config.yml.");
+            server.shutdown();
+            return;
+        }
+
         BukkitScheduler scheduler = server.getScheduler();
         EventsListener eventsListener = new EventsListener();
         TPSMeasurer tpsMeasurer = new TPSMeasurer();
-        StatsPoster statsPoster = new StatsPoster(new StatsFetcher(server, eventsListener, tpsMeasurer), getConfig().getString("serverToken"));
+        StatsFetcher statsFetcher = new StatsFetcher(server, eventsListener, tpsMeasurer);
+        StatsPoster statsPoster = new StatsPoster(statsFetcher, authorization);
 
         server.getPluginManager().registerEvents(eventsListener, this);
 
